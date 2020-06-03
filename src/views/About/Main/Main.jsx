312fc23906aad2317f42edae9b1cc2f8a109e0cc
Michael Williams <manicdesigner@gmail.com>
2020-06-03 19:22:55 -0400

    about fixed to center in all views

--- src/views/About/Main/Main.jsx
+++ src/views/About/Main/Main.jsx
@@ -5,26 +5,22 @@ const Main = () => {
 		<div className="main-container">
 			<h1 className="title">Crowdfunding for community projects you care about</h1>
 			<div className="main-content">
-				<div className="main-left">
-				</div>
-				<div className="main-right">
-					<p>
-						Modern and efficient infrastructure is foundational to ensuring communities have economic
-						opportunities well into the future. And yet, across the United States, one can find communities
-						with more infrastructure projects that need to be undertaken than their local community has
-						funds and resources.
-					</p>
-					<p>
-						Enter ReVitalize, a crowdsourcing & crowdfunding platform dedicated to serving stakeholders
-						interested in rejuvenating their community as well as trade professionals & apprentices who are
-						ready, willing, and able.
-					</p>
-					<p>
-						ReVitalize allows users to participate in community projects in a number of ways, whether they
-						want to create a project and raise funds, donate, organize available resources, or volunteer
-						based on their availability and desired skillset.
-					</p>
-				</div>
+				<p>
+					Modern and efficient infrastructure is foundational to ensuring communities have economic
+					opportunities well into the future. And yet, across the United States, one can find communities with
+					more infrastructure projects that need to be undertaken than their local community has funds and
+					resources.
+				</p>
+				<p>
+					Enter ReVitalize, a crowdsourcing & crowdfunding platform dedicated to serving stakeholders
+					interested in rejuvenating their community as well as trade professionals & apprentices who are
+					ready, willing, and able.
+				</p>
+				<p>
+					ReVitalize allows users to participate in community projects in a number of ways, whether they want
+					to create a project and raise funds, donate, organize available resources, or volunteer based on
+					their availability and desired skillset.
+				</p>
 			</div>
 		</div>
 	);
