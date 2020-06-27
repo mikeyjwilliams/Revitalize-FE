52ee311caf22c18434dda12bf00645ed5ac1db9a
Michael Williams <manicdesigner@gmail.com>
2020-06-27 13:27:58 -0400

    styling smaller screen tablet img full screen

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -45,74 +45,79 @@ const Login = props => {
 	return (
 		<>
 			<main className="login-container">
-
 				<FaArrowLeft onClick={goBack} className="back-arrow" />
 
-				<div className="login-container-left">
+				<div className="login-container-main imgContainer">
 					<div className="login-form">
 						<div className="login-third-party">
-							<div className="login-welcome">
-								<h1>Welcome Back!</h1>
-							</div>
-							<button>
-								<div>
-									<a className="login-button" href={`${process.env.REACT_APP_OAUTH_GOOGLE_LINK}`}>
-										<img src={googleLogo} alt="Google logo" />
-										<h2>Sign In With Google</h2>
-									</a>
+							<div className="login-card">
+								<div className="login-welcome">
+									<h1>Welcome Back!</h1>
 								</div>
-							</button>
-							<button>
-								<div>
-									<a className="login-button" href={`${process.env.REACT_APP_OAUTH_FACEBOOK_LINK}`}>
-										<img src={fbLogo} alt="Facebook logo" />
-										<h5 data-testid="h5">Sign In With Facebook</h5>
-									</a>
+								<button>
+									<div>
+										<a className="login-button" href={`${process.env.REACT_APP_OAUTH_GOOGLE_LINK}`}>
+											<img src={googleLogo} alt="Google logo" />
+											<h2>Sign In With Google</h2>
+										</a>
+									</div>
+								</button>
+								<button>
+									<div>
+										<a
+											className="login-button"
+											href={`${process.env.REACT_APP_OAUTH_FACEBOOK_LINK}`}
+										>
+											<img src={fbLogo} alt="Facebook logo" />
+											<h5 data-testid="h5">Sign In With Facebook</h5>
+										</a>
+									</div>
+								</button>
+
+								<div className="login-middle">
+									<div className="login-line"></div>
+									<p>or</p>
+									<div className="login-line"></div>
 								</div>
-							</button>
-						</div>
-						<div className="login-middle">
-							<div className="login-line"></div>
-							<p>or</p>
-							<div className="login-line"></div>
-						</div>
-						<form className="login-local" onSubmit={handleSubmit}>
-							<p className="login-title">Email</p>
-							<input
-								data-testid="email"
-								name='email'
-								type='email'
-								placeholder="Email..."
-								value={state.email}
-								onChange={handleChanges}
-							/>
-							<div className="login-pass">
-								<label htmlFor="password-login">
-									<p className="">Password</p>
-								</label>
-								{/* <span className="">Forgot Password?</span> */}
+								<form className="login-local" onSubmit={handleSubmit}>
+									<p className="login-title">Email</p>
+									<input
+										data-testid="email"
+										name="email"
+										type="email"
+										placeholder="Email..."
+										value={state.email}
+										onChange={handleChanges}
+									/>
+									<div className="login-pass">
+										<label htmlFor="password-login">
+											<p className="">Password</p>
+										</label>
+										{/* <span className="">Forgot Password?</span> */}
+									</div>
+									<input
+										data-testid="password"
+										name="password"
+										type="password"
+										placeholder="Password..."
+										value={state.password}
+										onChange={handleChanges}
+									/>
+									<div className="login-mid">
+										<p>
+											Don't have an account?{' '}
+											<Link to="/register" style={{ textDecoration: `underline` }}>
+												Create One
+											</Link>
+										</p>
+									</div>
+									<button data-testid="toggle">Log In</button>
+								</form>
 							</div>
-							<input
-							    data-testid="password"
-								name="password"
-								type="password"
-								placeholder="Password..."
-								value={state.password}
-								onChange={handleChanges}
-							/>
-							<div className="login-mid">
-								<p>
-									Don't have an account?{' '}
-									<Link to="/register" style={{ textDecoration: `underline` }}>
-										Create One
-									</Link>
-								</p>
-							</div>
-							<button data-testid="toggle">Log In</button>
-						</form>
+						</div>
 					</div>
 				</div>
-				<div className="imgContainer"></div>
+				{/* <div className="imgContainer"></div> */}
 			</main>
 		</>
 	);
