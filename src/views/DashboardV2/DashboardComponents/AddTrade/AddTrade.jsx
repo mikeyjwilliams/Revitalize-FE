08cdf6967702763024567919fec273cc41186900
littleonetwo <littleonetwo@gmail.com>
2020-07-06 21:27:10 -0500

    added validation methods to add trade but it is not finalized

--- src/views/DashboardV2/DashboardComponents/AddTrade/AddTrade.jsx
+++ src/views/DashboardV2/DashboardComponents/AddTrade/AddTrade.jsx
@@ -1,4 +1,4 @@
-import React, {  } from 'react';
+import React, { useState } from 'react';
 import { MdClose } from "react-icons/md";
 
 
@@ -12,7 +12,46 @@ const AddTrade = props => {
 
     } = props;
 
-    console.log("Props in addtrade", props)
+    const [errors, setErrors] = useState({
+  		validate:false,
+      validate2:false,
+      validate3:true
+  	});
+
+    const validateInput = e =>{
+      console.log("validation happens", e.target.name)
+      if (e.target.value == 0) {
+        if(e.target.name == "name"){
+          setErrors({
+            ...errors,
+            validate: true,})
+        } else if(e.target.name =="description") {
+          setErrors({
+            ...errors,
+            validate2:true,})
+        }
+      } else {
+        if(e.target.name == "name"){
+          setErrors({
+            ...errors,
+            validate: false,})
+        } else {
+          setErrors({
+            ...errors,
+            validate2:false,})
+        }
+
+      }
+      console.log("validation3 1:", errors.validate," 2:",errors.validate2, " 3:", errors.validate3)
+      if(errors.validate == true || errors.validate2 == true){
+        setErrors({...errors, validate3:true})
+      } else if (errors.validate == false && errors.validate2 == false){
+        setErrors({...errors,validate3:false})
+      }
+    }
+
+
+    console.log("are we validated?", errors.validate)
     return (
         <>
             <section
@@ -28,8 +67,12 @@ const AddTrade = props => {
                     <form onSubmit={submitAddTrade} >
                         <select
                             name='name'
-                            value={addTradeState.name}
-                            onChange={(event) => setAddTradeState({ ...addTradeState, [event.target.name]:event.target.value })}
+                            onChange={(event) => {
+                                validateInput(event);
+                                setAddTradeState({ ...addTradeState, [event.target.name]:event.target.value });
+
+                              }
+                            }
                         >
                           <option value={0} selected>Select One</option>
                           <option value="Electrician">Electrician</option>
@@ -40,14 +83,19 @@ const AddTrade = props => {
                           <option value="Landscaper">Landscaper</option>
                           <option value="Stone Mason">Stone Mason</option>
                         </select>
+                        {errors.validate && <p className="errorText">Please select a valid selection</p>}
                         <br />
 
                         <select
                             name='description'
                             type='text'
-                            placeholder='Description...'
                             value={addTradeState.description}
-                            onChange={(event) => setAddTradeState({ ...addTradeState, [event.target.name]:event.target.value })}
+                            onChange={(event) =>{
+                              validateInput(event);
+                              setAddTradeState({ ...addTradeState, [event.target.name]:event.target.value })
+
+                              }
+                            }
                         >
                           <option value={0} selected>How many do you need?</option>
                           <option value="You need 1">1</option>
@@ -56,11 +104,13 @@ const AddTrade = props => {
                           <option value="You need 4">4</option>
                           <option value="You need 5+">5+</option>
                         </select>
+                        {errors.validate2 && <p className="errorText">Please select a valid selection</p>}
                         <br />
                         <br />
 
                         <div className="add-trade-button-container" >
-                            <button className="add-trade-button" onClick={submitAddTrade} >
+
+                            <button className={`add-trade-button ${!errors.validate3 && 'disabled'}`}  >
                                 Submit
                             </button>
                         </div>
@@ -74,4 +124,4 @@ const AddTrade = props => {
 }
 
 
-export default AddTrade
+export default AddTrade;
