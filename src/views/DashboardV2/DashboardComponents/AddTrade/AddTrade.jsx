8f3d745257bec0cb47b06f34120bdf8d99cf6b98
littleonetwo <littleonetwo@gmail.com>
2020-07-06 20:13:00 -0500

    fixed some issues with darkmode modals in dashboard view and also changed add profession to be a drop ddown

--- src/views/DashboardV2/DashboardComponents/AddTrade/AddTrade.jsx
+++ src/views/DashboardV2/DashboardComponents/AddTrade/AddTrade.jsx
@@ -4,49 +4,68 @@ import { MdClose } from "react-icons/md";
 
 
 const AddTrade = props => {
-    const { 
-        setAddTradeState, 
-        addTradeState, 
+    const {
+        setAddTradeState,
+        addTradeState,
         submitAddTrade,
-        setAddTradeModal, 
+        setAddTradeModal,
 
     } = props;
 
     console.log("Props in addtrade", props)
     return (
         <>
-            <section 
-                className="add-trade-container" 
+            <section
+                className="add-trade-container"
             >
                 <div className="add-trade-card">
-                    
+
                     <div className="add-trade-actions">
-                        <MdClose onClick={() => setAddTradeModal({ show: false })} />
+                        <MdClose className="close" onClick={() => setAddTradeModal({ show: false })} />
                     </div>
 
                     <h3>Add Trade</h3>
                     <form onSubmit={submitAddTrade} >
-                        <input 
+                        <select
                             name='name'
-                            type='text'
-                            placeholder='Name...'
                             value={addTradeState.name}
                             onChange={(event) => setAddTradeState({ ...addTradeState, [event.target.name]:event.target.value })}
-                        />
-                        <input 
+                        >
+                          <option value={0} selected>Select One</option>
+                          <option value="Electrician">Electrician</option>
+                          <option value="Carpenter">Carpenter</option>
+                          <option value="Plumber">Plumber</option>
+                          <option value="HVAC technician">HVAC technician</option>
+                          <option value="Welder">Welder</option>
+                          <option value="Landscaper">Landscaper</option>
+                          <option value="Stone Mason">Stone Mason</option>
+                        </select>
+                        <br />
+
+                        <select
                             name='description'
                             type='text'
                             placeholder='Description...'
                             value={addTradeState.description}
                             onChange={(event) => setAddTradeState({ ...addTradeState, [event.target.name]:event.target.value })}
-                        />
+                        >
+                          <option value={0} selected>How many do you need?</option>
+                          <option value="You need 1">1</option>
+                          <option value="You need 2">2</option>
+                          <option value="You need 3">3</option>
+                          <option value="You need 4">4</option>
+                          <option value="You need 5+">5+</option>
+                        </select>
+                        <br />
+                        <br />
+
                         <div className="add-trade-button-container" >
                             <button className="add-trade-button" onClick={submitAddTrade} >
                                 Submit
                             </button>
                         </div>
                     </form>
-                  
+
                 </div>
 
             </section>
@@ -56,7 +75,3 @@ const AddTrade = props => {
 
 
 export default AddTrade
-
-
-
-
